79fc71422b9a69bb47dc06153f9696cd3a486892
Moiz.Kachwala <moizk@hotmail.com>
2016-06-09 12:35:15 +0530

    fixed warnings

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,5 +1,5 @@
 /* Avoid: 'error TS2304: Cannot find name <type>' during compilation */
-///<reference path="../../typings/globals/es6-shim/index.d.ts"/>
+///<reference path="../typings/globals/es6-shim/index.d.ts"/>
 
 import {bootstrap} from 'angular2/platform/browser';
 
