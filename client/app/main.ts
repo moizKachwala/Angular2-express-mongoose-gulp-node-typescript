7a970b045222b819365686ca64de618fd33d1549
Moiz.Kachwala <moizk@hotmail.com>
2016-08-26 16:44:59 +0530

    removed unwanted typings

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,6 +1,3 @@
-/* Avoid: 'error TS2304: Cannot find name <type>' during compilation */
-///<reference path="../typings/globals/es6-shim/index.d.ts"/>
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
--- client/typings/index.d.ts
+++ client/typings/index.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
